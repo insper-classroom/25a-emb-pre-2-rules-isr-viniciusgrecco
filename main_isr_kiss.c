947f10455a8308ab75b98c057e8bc4ebf47c5d77
Vinicius Grecco <viniciusgfm@al.insper.edu.br>
2025-02-18 11:26:05 -0300

    3

--- main_isr_kiss.c
+++ main_isr_kiss.c
@@ -1,8 +1,6 @@
 #include "asf.h"
 
 volatile int g_cnt = 0;
-char g_str[10];
-int batata;
 
 // This code creates a progress bar on an OLED screen that
 // increases when the button is pressed.
@@ -18,6 +16,8 @@ void btn_callback(void) {
 
 void main(void) {
   // ...
+  char g_str[10];
+  int batata; 
 
   batata = batata + 1;
   while (1) {
