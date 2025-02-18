f455799ea8d34e5bee2b41d7411856c98866b3eb
Vinicius Grecco <viniciusgfm@al.insper.edu.br>
2025-02-18 12:31:13 -0300

    23

--- main_isr_practice.c
+++ main_isr_practice.c
@@ -6,7 +6,7 @@ const int BTN_PIN_R = 28;
 
 // ERRO: cppcheck unitvar
 // ERRO: addon IsrVarVolatile
-int btn_flag;
+volatile int btn_flag;
 
 void btn_callback(uint gpio, uint32_t events) {
   if (events == 0x4) { // fall edge
